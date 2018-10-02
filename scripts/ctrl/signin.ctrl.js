ddc31a5ee5ba94196852159c771b929e3b3b2bfc
Jasmin Tiric <jasmin.tiric@gmail.com>
2018-10-02 17:32:32 +0200

    Landing page: HTML

---
+++ scripts/ctrl/signin.ctrl.js
@@ -0,0 +1,7 @@
+var app = angular.module('bookStoreApp')
+
+app.controller('signInCtrl' , function ($scope) {
+    $scope.checkCustomerName = function (user) {
+        console.log(user);
+    }
+})
