a8101583bba3b472d5ecb9b3cb89793dab6acccc
Jasmin Tiric <jasmin.tiric@gmail.com>
2018-10-11 13:19:38 +0200

    Author - view and ctrl

---
+++ scripts/ctrl/author.ctrl.js
@@ -0,0 +1,6 @@
+var app = angular.module('bookStoreApp')
+app.controller('authorCtrl', function ($scope, $uibModalInstance, dataService, authorId){
+    dataService.read("authors", authorId, function (data) {
+        $scope.author = data;
+    })
+})
--- scripts/ctrl/authors.ctrl.js
+++ scripts/ctrl/authors.ctrl.js
@@ -1,7 +1,17 @@
 var app = angular.module('bookStoreApp');
-app.controller('authorsCtrl', function ($scope, dataService) {
+app.controller('authorsCtrl', function ($scope, dataService, $uibModal) {
 
     dataService.list("authors", function (data) {
         $scope.authors = data;
     })
+
+    $scope.showAuthor = function (id) {
+        var modalInstance = $uibModal.open({
+            animation: true,
+            templateUrl: 'view/author.html',
+            controller: 'authorCtrl',
+            size: 'xl',
+            resolve: { authorId: id},
+        })
+    }
 })
